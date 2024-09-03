64983295dfa3ff6c553d49078527239370e3f2c3
Artur Mostowski <artur.mostowski@protonmail.com>
2024-09-03 17:40:18 +0200

    code

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # slack-reactions
+
+## How to use
+
+1. Get a token as instructed here https://stackoverflow.com/a/67795540/7195666
+2. Create `.env` file with `TOKEN=your-token`
+3. `npm install`
+4. `node index.js <channel-id> <string> <text of reactions>`
+
+string: should be a string that appears in the message you want to react to and no later message.
+
+## Prerequisites
+
+This code assumes you have 2 letter emoji packs `alphabet-${colour}-${reaction}` for colours `white` and `yellow` and reaction being letters.
+
+TODO: more alphabets
---
+++ index.js
@@ -0,0 +1,68 @@
+import axios from 'axios'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const token = process.env.TOKEN
+const channelName = process.argv[2]
+const text = process.argv[3]
+const reactions = process.argv[4]
+
+async function main() {
+    const letterCounts = reactions.split('').reduce((acc, letter) => {
+        const count = acc[letter] || 0
+        return {
+            ...acc,
+            [letter]: count + 1
+        }},
+        {})
+    const maxCount = Math.max(...Object.values(letterCounts))
+    if (maxCount > 2) {
+        console.log('You can only use each reaction twice')
+        process.exit(1)
+    }
+
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+
+    const channelsResponse = await axios.post('https://slack.com/api/conversations.list', {}, config)
+    // console.log(JSON.stringify(channelsResponse.data, null, 2))
+    // const channel = channelsResponse.data.channels.find(channel => channel.name === channelName)
+    const channelId = channelName
+    console.log({ channelId })
+    const messages = (await axios.post('https://slack.com/api/conversations.history', {
+        channel: channelId
+    }, config)).data.messages
+    console.log({messagesLen: messages.length})
+    // console.log('------------------')
+    // console.log(JSON.stringify(messages.data, null, 2))
+    console.log(
+        messages.map(m => m.text.slice(0, 10))
+    )
+    console.log({text})
+    const regex = new RegExp(`${text}`, 'i')
+    const message = messages.find(m => m.text.match(regex))
+    console.log(
+        messages.map(m => m.text.match(regex))
+    )
+    console.log({message})
+    const used = {}
+    for (const reaction of reactions) {
+        const colour = used[reaction] ? 'yellow' : 'white'
+        const name = `alphabet-${colour}-${reaction}`
+        const response = await axios.post('https://slack.com/api/reactions.add', {
+            channel: channelId,
+            name,
+            timestamp: message.ts
+        }, config)
+        used[reaction] = true
+    }
+}
+
+main()
+
+
+
